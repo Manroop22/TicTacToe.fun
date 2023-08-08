f1c76ed18a7e9b8f65025bdfc8bd9ae878b08ea5
Manroop22 <77290018+Manroop22@users.noreply.github.com>
2023-08-08 17:45:15 +0530

    JS started. Created the changeTurn and the checkWin method

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log("Welcome to playing Tic Tac Toe")
+let turn =new Audio("ping.wav")
+let win=new Audio("GameOverSound.wav")
+let turnValue ="X"
+
+
+// Function to change the turn.
+const changeTurn= ()=>{
+    return turnValue==="X"?"O":"X";
+}
+
+// Fucntion to check for a Win.
+const checkWin= ()=>{
+
+}
+
+// Game Logic
